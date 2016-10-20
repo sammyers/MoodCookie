d85ba0d71211308f9688d6a92ac3539ae1adeefc
Cecelia Auerswald <cecelia.auerswald@gmail.com>
2016-10-20 18:35:58 -0400

    ADD HomepageFragment

--- MoodCookie/app/src/main/java/cecelia/moodcookie/MainActivity.java
+++ MoodCookie/app/src/main/java/cecelia/moodcookie/MainActivity.java
@@ -1,5 +1,7 @@
 package cecelia.moodcookie;
 
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -25,5 +27,15 @@ public class MainActivity extends AppCompatActivity {
         for (Note note : notes) {
             Log.d("Main Activity", note.getText());
         }
+
+        addMainPageFragment();
+
+    }
+
+    public void addMainPageFragment() {
+        FragmentManager fragmentManager = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_holder, new HomepageFragment(), "CURRENT_FRAGMENT");
+        fragmentTransaction.commit();
     }
 }
