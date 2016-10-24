52b01fab29313d91dae12c0c58e3f1a6fd174110
mary-keenan <mary.keenan@students.olin.edu>
2016-10-24 00:14:14 -0400

    EDIT/ADD: Indico API set up, needs to be integrated

--- MoodCookie/app/src/main/java/cecelia/moodcookie/IndicoCrap.java
+++ MoodCookie/app/src/main/java/cecelia/moodcookie/IndicoCrap.java
@@ -1,20 +1,26 @@
 package cecelia.moodcookie;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Base64;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import java.io.IOException;
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
 
-import io.indico.Indico;
-//import io.indico.api.image.FacialEmotion;
-import io.indico.enums.FacialEmotion;
-import io.indico.results.IndicoResult;
-import io.indico.utils.IndicoException;
-import io.indico.network.IndicoCallback;
+import java.io.ByteArrayOutputStream;
+import java.util.HashMap;
+import java.util.Map;
+
+import cecelia.moodcookie.types.MySingleton;
 
 
 public class IndicoCrap extends Fragment {
@@ -29,46 +35,59 @@ public class IndicoCrap extends Fragment {
         // Inflate the layout for this fragment
         final View view = inflater.inflate(R.layout.fragment_indico_experimenting, container, false);
 
-//        ImageView sampleIV = (ImageView) view.findViewById(R.id.samplePic);
-        String APIKey = "8f9117c50bd2909f3c13fb5ab04ea528";
-        final String filePath = "/drawable/happy_lady.jpg";
+        final String APIKey = BuildConfig.API_KEY;
         final TextView output = (TextView) view.findViewById(R.id.sampleOutput);
+        String url = "https://apiv2.indico.io/fer"; //url for Indico's facial emotion recognition
 
-        Indico.init(getContext(),"8f9117c50bd2909f3c13fb5ab04ea528", null); //nothing happened here
-
-        Log.d("RUH ROH1", "**************************");
-        try {
-            Indico.sentiment.predict("indico is so easy to use!", new IndicoCallback<IndicoResult>() {
-                @Override public void handle(IndicoResult result) throws IndicoException {
-                    Log.d("RUH ROH2", "**************************");
-                    Log.i("Indico Sentiment", "sentiment of: " + result.getSentiment());
-                    output.setText(result.getSentiment().toString());
+        StringRequest postRequest=new StringRequest(Request.Method.POST,url,new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                HashMap<String, Float> emotionDict = new HashMap(); //dictionary we store emotions and their values in
+                Float max_val = Float.parseFloat("0");
+                String max_emotion = "";
+                String shortenedResponse = response.substring(13, response.length()-2).replaceAll("\"",""); //strip "{"results": {", "}", and " from original response
+                String[] splitResponse = shortenedResponse.split(", "); //split emotions into a list of emotions + their corresponding values
+                for (int i = 0; i < splitResponse.length; i++){ //loops through emotions
+                    String[] separated = splitResponse[i].split(": "); //split emotion and corresponding value, creates list of length 2 (key, value)
+                    String key = separated[0];
+                    Float val = Float.parseFloat(separated[1]); //convert from string into float
+                    if (val > max_val){ //this conditional will tell us what emotion is most prevalent in the image
+                        max_val = val;
+                        max_emotion = key;
+                    }
+                    emotionDict.put(key,val); //put key/value pairs in map
                 }
-            });
-        } catch (IOException | IndicoException e) {
-            Log.d("RUH ROH4", "**************************");
-            e.printStackTrace();
+
+                output.setText("You are " + max_emotion.toLowerCase()); //set emotion to text
+                Log.d("EDITED*****:",shortenedResponse);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError volleyError) {
+                Log.d("error:",volleyError.toString());
+
+            }
+        }){@Override
+        protected Map<String, String> getParams() throws AuthFailureError {
+            Map<String,String> params= new HashMap<>(); //only params are data (picture in base64)
+            Bitmap bm = BitmapFactory.decodeResource(getResources(),R.drawable.happy_lady); //decode picture
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            bm.compress(Bitmap.CompressFormat.JPEG, 100, baos); //compress data stream
+            byte[] byteArray = baos.toByteArray();
+            String encodedImage = Base64.encodeToString(byteArray, Base64.DEFAULT); //create string via Base64
+            params.put("data",encodedImage); //put data into params list
+            return params;
         }
-        Log.d("RUH ROH5", "**************************");
 
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String,String> headers= new HashMap<>();
+                headers.put("X-APIKey", APIKey); //Indico requires this as of August 1st
+                return headers;
+            }
+        };
 
-//        try {
-//            Log.d("ABOUT TO TRY",APIKey);
-//            Indico.fer.predict(filePath, new IndicoCallback<IndicoResult>() { //pass in picture and get output
-//                @Override public void handle(IndicoResult result) throws IndicoException {
-//                    //create map of facial emotions to doubles
-//                    Map<FacialEmotion, Double> resultMap = result.getFer();
-//                    String sampleText = resultMap.get("Happy").toString();
-//                    //set output to text view
-//                    Log.d("ABOUT TO SET TEXT",sampleText);
-//                    output.setText("BOB");
-//                }
-//            });
-//        } catch (IOException | IndicoException e) {
-//            Log.d("ABOUT TO PRINT ERROR","You done fucked up");
-//            e.printStackTrace();
-//        }
-
+        MySingleton.getInstance(getActivity()).addToRequestQueue(postRequest);
 
         return view;
     }
