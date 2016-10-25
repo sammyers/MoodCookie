d1b0984d8e2b9b5095f7f34660a34c43c53c452d
sammyers <sam@students.olin.edu>
2016-10-25 16:16:06 -0400

    Now displays the actual photo in the display fragment. Hooked up some more buttons.

--- MoodCookie/app/src/main/java/cecelia/moodcookie/DisplayPageFragment.java
+++ MoodCookie/app/src/main/java/cecelia/moodcookie/DisplayPageFragment.java
@@ -6,6 +6,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 
 /**
  * Created by Cecelia on 10/24/16.
@@ -14,13 +15,19 @@ import android.widget.ImageButton;
 public class DisplayPageFragment extends Fragment {
 
     private ImageButton backButton;
+    private MainActivity mainActivity;
+    private ImageView imageView;
 
     public DisplayPageFragment() {}
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, final Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.display_fragment, container, false);
 
-        this.backButton = (ImageButton) view.findViewById(R.id.back_to_homepage);
+        backButton = (ImageButton) view.findViewById(R.id.back_to_homepage);
+        mainActivity = (MainActivity) getActivity();
+        imageView = (ImageView) view.findViewById(R.id.display_image_view);
+
+        mainActivity.getPhotoHandler().setPhoto(imageView);
 
         setOnClickListeners();
 
@@ -31,8 +38,7 @@ public class DisplayPageFragment extends Fragment {
         backButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                MainActivity activity = (MainActivity) getActivity();
-                activity.startHomepageFragment();
+                mainActivity.startHomepageFragment();
             }
         });
     }
